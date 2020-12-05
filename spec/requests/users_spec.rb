fe4a4350866ca8a9561ac2b2d1d4b255f874a82f
Sylor-huang <sylor_huang@126.com>
2020-12-05 16:07:51 +0800

    complete requests_users_spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,77 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  # describe "#index" do
-  #   context 'when user is signned in' do
-  #     subject { get users_path }
+  describe "#index" do
+    context 'when user is signned in' do
+      subject { get users_path }
 
-  #     before do
-  #       @user = create(:user, activated: true, activated_at: Time.zone.now)
-  #     end
+      before do
+        @user = create(:user, activated: true, activated_at: Time.zone.now)
+      end
 
-  #     it 'retunrs a 200 response' do
-  #       sign_in_as(@user)
-  #       subject
-  #       expect(response).to have_http_status(200)
-  #     end
+      it 'retunrs a 200 response' do
+        sign_in_as(@user)
+        subject
+        expect(response).to have_http_status(200)
+      end
 
-  #     it 'render index template' do
-  #       sign_in_as(@user)
-  #       expect(subject).to render_template(:index)
-  #     end
-  #   end
+      it 'render index template' do
+        sign_in_as(@user)
+        expect(subject).to render_template(:index)
+      end
+    end
 
-  #   context 'when user is not signned in' do
-  #     subject { get users_path }
+    context 'when user is not signned in' do
+      subject { get users_path }
 
-  #     it 'retunrs a 302 response' do
-  #       subject
-  #       expect(response).to have_http_status(302)
-  #     end
+      it 'retunrs a 302 response' do
+        subject
+        expect(response).to have_http_status(302)
+      end
 
-  #     it 'render index template' do
-  #       expect(subject).to redirect_to(login_url)
-  #     end
-  #   end
-  # end
+      it 'render index template' do
+        expect(subject).to redirect_to(login_url)
+      end
+    end
+  end
 
-  # describe "#edit" do
-  #   context 'when user is signned in' do
-  #     before do
-  #       @user = create(:user, activated: true, activated_at: Time.zone.now)
-  #     end
+  describe "#edit" do
+    context 'when user is signned in' do
+      before do
+        @user = create(:user, activated: true, activated_at: Time.zone.now)
+      end
 
-  #     subject { get edit_user_path(@user) }
+      subject { get edit_user_path(@user) }
 
-  #     it 'retunrs a 200 response' do
-  #       sign_in_as(@user)
-  #       subject
-  #       expect(response).to have_http_status(200)
-  #     end
+      it 'retunrs a 200 response' do
+        sign_in_as(@user)
+        subject
+        expect(response).to have_http_status(200)
+      end
 
-  #     it 'render edit template' do
-  #       sign_in_as(@user)
-  #       expect(subject).to render_template(:edit)
-  #     end
-  #   end
+      it 'render edit template' do
+        sign_in_as(@user)
+        expect(subject).to render_template(:edit)
+      end
+    end
 
-  #   context 'when user is not signned in' do
-  #     before do
-  #       @user = create(:user, activated: true, activated_at: Time.zone.now)
-  #     end
+    context 'when user is not signned in' do
+      before do
+        @user = create(:user, activated: true, activated_at: Time.zone.now)
+      end
 
-  #     subject { get edit_user_path(@user) }
+      subject { get edit_user_path(@user) }
 
-      # it 'returns a 302 response' do
-      #   subject
-      #   expect(response).to have_http_status(302)
-      # end
+      it 'returns a 302 response' do
+        subject
+        expect(response).to have_http_status(302)
+      end
 
-  #     it 'render index template' do
-  #       expect(subject).to redirect_to(login_url)
-  #     end
-  #   end
-  # end
+      it 'render index template' do
+        expect(subject).to redirect_to(login_url)
+      end
+    end
+  end
 
   describe '#show' do
     
@@ -107,6 +107,7 @@ RSpec.describe "Users", type: :request do
 
     it 'should save user' do 
       post users_path, params: { user: user_params }
+      expect(flash[:info]).to eq("Please check your email to activate your account.")
       expect(response).to have_http_status(302)
     end
 
@@ -117,18 +118,42 @@ RSpec.describe "Users", type: :request do
   end
 
   describe '#destroy' do
-    let(:user) { create(:user, activated: true, activated_at: Time.zone.now) }
+    let(:test_user) { create(:user, activated: true, activated_at: Time.zone.now) }
+    let(:admin_user) { create(:user, activated: true, admin: true, activated_at: Time.zone.now) }
+
+    subject { delete user_path(test_user)  }
+
+    it 'should redirect to login ' do 
+      subject
+      expect(subject).to redirect_to login_path
+    end
+
+    it 'should redirect to root_path ' do 
+      sign_in_as(test_user)
+      subject
+      expect(subject).to redirect_to root_path
+    end
 
     it 'should return success' do 
-      delete user_path(user.id) 
+      sign_in_as(admin_user)
+      subject
       expect(flash[:success]).to eq("User deleted")
-      expect(response).to have_http_status(302)
-     
+      expect(subject).to redirect_to users_url
     end
   end
 
-  # describe '#following' do
-  #   # TODO
-  # end
+  describe '#following' do
+    let(:user) { create(:user, activated: true, activated_at: Time.zone.now) }
+    let(:user_1) { create(:user, activated: true, activated_at: Time.zone.now) }
+    let!(:relationship) { create(:relationship, follower_id: user.id, followed_id: user_1.id) }
+
+    it 'should be successful' do 
+      sign_in_as(user)
+      get following_user_path(user)
+      expect(assigns(:users).size).to  eq(1)
+      expect(response).to  render_template "show_follow"
+    end
+
+  end
   
 end
