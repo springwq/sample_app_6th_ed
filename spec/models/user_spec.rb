0d37c8e309105570f31f134bd2c7d8a72aae57d7
Sylor-huang <sylor_huang@126.com>
2020-12-05 16:34:35 +0800

    complete requests_users_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe User, type: :model do
   end
 
   describe '#send_password_reset_email' do
-    let(:user) { User.new(email: "sylor@feedmob.com", password: "sylor12345",name: "sylor") }
+    let(:user) { User.new(email: Faker::Internet.email, password: SecureRandom.hex(4), name: Faker::Name.name) }
 
     it 'should send email' do 
       allow(UserMailer).to receive_message_chain(:password_reset, :deliver_now)
@@ -28,6 +28,65 @@ RSpec.describe User, type: :model do
 
   end
 
+  describe "#validates" do
+    context 'validates nil name' do 
+      let(:user) { User.new(email: Faker::Internet.email, password: SecureRandom.hex(4), name: "") }
+      it 'should return errors' do 
+        result = user.save
+        expect(result).to be false
+      end
+    end
+
+    context 'validates too long name' do 
+      let(:user) { User.new(email: Faker::Internet.email, password: SecureRandom.hex(4), name: SecureRandom.hex(30)) }
+      it 'should return errors' do 
+        result = user.save
+        expect(result).to be false
+      end
+    end
+
+    context 'validates valid name' do 
+      let(:user) { User.new(email: Faker::Internet.email, password: SecureRandom.hex(4), name: SecureRandom.hex(3)) }
+      it 'should be success' do 
+        result = user.save
+        expect(result).to be true
+      end
+    end
+
+    context 'validates invalid email' do 
+      let(:user) { User.new(email: "test_email", password: SecureRandom.hex(4), name: SecureRandom.hex(3)) }
+      it 'should return errors' do 
+        result = user.save
+        expect(result).to be false
+      end
+    end
+
+    context 'validates nil email' do 
+      let(:user) { User.new(email: "", password: SecureRandom.hex(4), name: SecureRandom.hex(3)) }
+      it 'should return errors' do 
+        result = user.save
+        expect(result).to be false
+      end
+    end
+
+    context 'validates exists email' do 
+      let!(:user) { create(:user, email: "test_email@126.com") }
+      let(:new_user) { User.new(email: "test_email@126.com", password: SecureRandom.hex(4), name: SecureRandom.hex(3)) }
+      it 'should return errors' do 
+        result = new_user.save
+        expect(result).to be false
+      end
+    end
+  end
+
+  describe User do
+    it {  should respond_to(:microposts) }
+    it {  should respond_to(:active_relationships) }
+    it {  should respond_to(:passive_relationships) }
+    it {  should respond_to(:following) }
+    it {  should respond_to(:followers) }
+  end
+  
   describe '#feed' do
     let(:user) { create(:user) }
 
