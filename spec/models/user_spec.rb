69459a7f54ac3aed17288ef3ef1626bb219c13af
circlesoar <circlesoar@gmail.com>
2020-12-08 08:33:15 +0000

    add rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,30 @@ RSpec.describe User, type: :model do
       expect(user.save).to be false
     end
   end
+
+  # TODO: Add tests for associations
+
+  # TODO: Add tests for validations
+
+  describe '#send_password_reset_email' do
+    let(:user) { User.new(name: "circle", email: "circle@feedmob.com", password: "123456") }
+    
+    it "send password reset email" do
+      allow(UserMailer).to receive(:password_reset)
+      result = user.save
+      expect(result).to eq(true)
+    end
+  end
+
+  describe '#feed' do
+    let(:user) { create(:user) }
+    
+    it 'should equal 0' do 
+      expect(user.feed.size).to eq(0)
+    end
+    
+    context "micopost " do
+      
+    end
+  end
 end
