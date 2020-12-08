3f515c04c838f6ded082737e3ff7f2b9080db202
circlesoar <circlesoar@gmail.com>
2020-12-08 07:41:46 +0000

    add rspec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -73,37 +73,70 @@ RSpec.describe "Users", type: :request do
     end
   end
 
-  describe '#show' do
-     context 'when user is signned in' do
-       
-      before do
-        @user = create(:user, activated: true, activated_at: Time.zone.now)
-      end
+  describe '#show' do 
+    before do
+      @user = create(:user, activated: true, activated_at: Time.zone.now)
+      sign_in_as(@user)
+    end
 
-      subject { get user_path(id: @user.id) }
+    subject { get user_path(@user) }
 
-      it "retunrs a 200 response" do
-        sign_in_as(@user)
-        subject
-        expect(response).to have_http_status(200)
-      end
-
-      it 'render show template' do
-        sign_in_as(@user)
-        expect(subject).to render_template(:show)
-      end
+    it 'should get user success' do 
+      subject
+      expect(response).to have_http_status(200)
+      expect(@user.microposts.reload.size).to eq(0)
     end
   end
 
   describe '#create' do
     # TODO
+    context "success" do
+      
+      it 'should return success' do 
+        post users_path, params: { user: {
+          name: "test",
+          password: "123123",
+          email: "test@email.com"
+        } }
+        expect(response).to have_http_status(302)
+      end
+    end
+    
+    context "fail" do
+      it 'should return fail' do 
+        post users_path, params: { user: {
+          name: "test",
+          password: "123123"
+        } }
+        expect(subject).to render_template(:new)
+      end
+    end
   end
+  
 
   describe '#destroy' do
     # TODO
+    let(:user_delete) { create(:user, activated: true, activated_at: Time.zone.now) }
+
+    subject { delete user_path(user_delete)  }
+
+    it 'should return success' do 
+      sign_in_as(user_delete)
+      subject
+      expect(flash[:success]).to eq("User deleted")
+    end
   end
 
   describe '#following' do
     # TODO
+    let(:user) { create(:user, activated: true, activated_at: Time.zone.now) }
+    let(:user_following) { create(:user, activated: true, activated_at: Time.zone.now) }
+
+    it 'should be successful' do 
+      sign_in_as(user)
+      get following_user_path(user)
+      expect(response).to render_template "show_follow"
+    end
   end
+  
 end
