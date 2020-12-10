b7de63c165758b66b3bb152f536150c5ba71b9ae
kaiji <762907794@qq.com>
2020-12-10 16:59:08 +0800

    add model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,25 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:password) }
   end
+
+  describe '#feed' do
+    let(:user) { create(:user) }
+
+    it 'return []' do
+      expect(user.feed.any?).to equal(false)
+    end
+
+    context "when user have micopost" do
+      let(:user) { create(:user) }
+      let(:followed_user) { create(:user) }
+      before do
+        create(:relationship, follower_id: user.id, followed_id: followed_user.id)
+        create(:micropost, user_id: user.id, content: "test")
+      end
+
+      it 'get feed' do
+        expect(user.feed.any?).to eq true
+      end
+    end
+  end
 end
