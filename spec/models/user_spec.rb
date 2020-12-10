86a65d26777a65df8ded0c6a444e74abd834fa17
steven <wuqi0509@gmail.com>
2020-12-10 10:39:16 +0800

    Add tests for associations and validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,15 +5,17 @@ RSpec.describe User, type: :model do
     expect(create(:user)).to be_valid
   end
 
-  context 'when name is not present' do
-    let(:user) { described_class.new(email: Faker::Internet.email, password: described_class.digest('password')) }
+  describe 'associations' do
+    it { is_expected.to have_many(:microposts).dependent(:destroy) }
+    it { is_expected.to have_many(:active_relationships).with_foreign_key('follower_id').class_name('Relationship').dependent(:destroy)  }
+    it { is_expected.to have_many(:passive_relationships).with_foreign_key('followed_id').class_name('Relationship').dependent(:destroy)  }
+    it { is_expected.to have_many(:following).through(:active_relationships).source(:followed) }
+    it { is_expected.to have_many(:followers).through(:passive_relationships).source(:follower) }
+  end
 
-    it 'is inivalid' do
-      expect(user.valid?).to be false
-    end
-
-    it 'is failed to save' do
-      expect(user.save).to be false
-    end
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name)}
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
   end
 end
