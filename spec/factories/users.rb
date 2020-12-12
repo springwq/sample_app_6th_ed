7cec0fd3da86eb2499ffd643214f60a972172431
kaiji <762907794@qq.com>
2020-12-12 15:27:44 +0800

    add model test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,14 @@
 FactoryBot.define do
-  FactoryBot.define do
-    factory :user, class: User do
-      name  { Faker::Name.name }
-      email { Faker::Internet.email }
-      password { 'password' }
-    end
-    factory :test_user, class: User do
-      name  { Faker::Name.name }
-      email { Faker::Internet.email }
-      password { 'password' }
-      activated { true }
-      activated_at { Time.zone.now }
-    end
+  factory :user, class: User do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { "password" }
+  end
+  factory :test_user, class: User do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { "password" }
+    activated { true }
+    activated_at { Time.zone.now }
   end
 end
